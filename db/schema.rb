1d3101529ce36544bdf4419995d767369677cc24
Airam <82757584+Airam9598@users.noreply.github.com>
2021-12-22 18:31:02 +0000

    Últimas correcciones de errores

--- db/schema.rb
+++ db/schema.rb
@@ -58,10 +58,9 @@ ActiveRecord::Schema.define(version: 2021_12_20_101622) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email", default: "", null: false
+    t.string "email"
     t.string "password_digest"
     t.boolean "admin"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
